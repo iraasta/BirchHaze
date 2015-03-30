1e95566f9232dd9789310b23641712c3158d8cdf
Maciej Galos <clooss@o2.pl>
2015-03-30 10:29:43 +0200

    wersja zapasowa

--- app/src/main/java/DataBase/City.java
+++ app/src/main/java/DataBase/City.java
@@ -12,8 +12,43 @@ public class City {
     private int population;
     private ArrayList<DayForecast> forecasts;
 
+
+
+
+    public void sort(){
+        String tmp="";
+        ArrayList<DayForecast> sortedForecasts = new ArrayList<DayForecast>();
+
+        ArrayList<DayForecast> qwe = forecasts;
+        forecasts = new ArrayList<DayForecast>();
+
+
+        if(qwe.size()>0){
+            forecasts.add(qwe.get(0));
+            for(int i=1 ; i<qwe.size() ; i++){
+                if((qwe.get(i).getDate_time_text().toCharArray())[11]=='1')
+                    forecasts.add(qwe.get(i));
+            }
+
+
+
+            sortedForecasts.add(forecasts.get(0));
+            tmp = forecasts.get(0).getDate_time_text();
+            for(int i=1;i<forecasts.size();i++){
+                if(((tmp.toCharArray())[9]!=(forecasts.get(i).getDate_time_text().toCharArray())[9]))
+                        sortedForecasts.add(forecasts.get(i));
+
+                tmp = forecasts.get(i).getDate_time_text();
+            }
+        }
+
+        forecasts = sortedForecasts;
+
+    }
+
     public City(){
         forecasts = new ArrayList<DayForecast>();
+
     }
 
     public ArrayList<DayForecast> getForecasts() {
